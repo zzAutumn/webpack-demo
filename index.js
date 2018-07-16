6f9c4c4e6f2f345d85d09e3ec07b98e50c87ce4a
zzAutumn <yjjjng0901@163.com>
2018-07-16 22:44:50 +0800

    webpack config

---
+++ index.js
@@ -0,0 +1,3 @@
+const { msg } = require('./script-2');
+
+alert(msg);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+
+};
